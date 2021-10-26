8a410d3886ea6bcac43c9f66857abe0637e823a7
Matthew Brown <matthewbrown@gatech.edu>
2021-10-26 15:31:16 -0400

    Update commit amount bounds

--- scripts/main.py
+++ scripts/main.py
@@ -17,7 +17,7 @@ if CONST.REPO_NAME not in [repo.name for repo in user.get_repos()]:
     
 repo = user.get_repo(CONST.REPO_NAME)
 
-for commits in range(random.randint(1, 15)):
+for commits in range(random.randint(1, 20)):
     try:
         contents = repo.get_contents(CONST.FILE_NAME)
         commit = repo.delete_file(contents.path, "Remove test", contents.sha, branch=CONST.BRANCH_NAME)
